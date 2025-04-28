6fb8cbe05ad41a4784c527566ce708aaab175330
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-04-28 18:24:08 +0700

    membuat karakter

--- Percobaan1.cpp
+++ Percobaan1.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class Planet{
+class Planet
+{
     private:
             float gravitasi;
     public:
-       
+    string nama;
+    float diameter;
 
+    void displaydata()
+    {
+        cout << "Hallo aku bumi" << nama << ", ukuranku " << diameter << ",gravitasiku " << gravitasi << endl;
+
+    }
+    void setdata(float g)
+    {
+        gravitasi = g;
+    }
 };
 
 int main()
